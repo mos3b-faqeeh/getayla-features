a2aafb060bdbad7b2f56bca726afa5e5e31c12c2
Mahmoud Laban <Mahmoud.Laban@gmail.com>
2021-07-11 18:13:45 +0100

    changed sqlite to postgreSQL

--- GetAylaDev/settings.py
+++ GetAylaDev/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'GetAylaDev.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'myproject',
-#         'USER': 'mosab',
-#         'PASSWORD': 'FAQEEH1990k',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'mosab',
+        'PASSWORD': 'FAQEEH1990k',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
